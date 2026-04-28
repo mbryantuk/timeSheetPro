e39c2c5cd010c60af27e6d4bf3aeac4ad60f29ac
matt <matt@homeserver>
2026-04-28 14:44:23 +0100

    fix: import axios in index.js to resolve 500 error

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const cron = require('node-cron');
+const axios = require('axios');
 const { db, initDb } = require('./db');
 const { summarizeActivities, abortGeneration } = require('./ollama');
 
